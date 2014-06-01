649421c533c5eb54cc5f314fe44d193eb3acc90a
George Lewis <ghl227@gmail.com>
2014-05-31 18:18:41 -0700

    Adding tests

---
+++ tests/test_plotting.py
@@ -0,0 +1,24 @@
+
+
+import pandas
+
+import bamboo.plotting
+
+group = ['A', 'A', 'A', 'A',
+         'B', 'B',
+         'C']
+
+feature1 = [1, 1, 1, 1,
+            2, 2,
+            3]
+
+feature2 = [10.0, 10.5, 9.5, 11.0,
+            20.0, 20.0,
+            0.0]
+
+df = pandas.DataFrame({'group':group,
+                       'feature1':feature1,
+                       'feature2':feature2})
+
+def test_boxplot():
+    bamboo.plotting.boxplot(df, 'feature1', 'feature2')
