0d75927b1f07214eed843b02f3834e85bda3e794
George Lewis <ghl227@gmail.com>
2014-08-24 14:47:42 -0700

    Cleaning up Bamboo class

--- bamboo.py
+++ bamboo.py
@@ -63,6 +63,7 @@ class Bamboo(object):
             return func(self.obj, *args, **kwargs)
         return callable
 
+
     def __getattr__(self, *args, **kwargs):
 
         name, args = args[0], args[1:]
@@ -71,53 +72,11 @@ class Bamboo(object):
 
         if (self._is_data_frame or self._is_group_by ) and name in _bamboo_methods:
             return self.create_callable(_bamboo_methods[name])
-            #return lambda : _bamboo_methods[name](self.obj, *args, **kwargs)
 
-        if self._is_data_frame and name in _frames_methods:            
+        if self._is_data_frame and name in _frames_methods:
             return self.create_callable(_frames_methods[name])
-
-            #print "Found frames func"
-            #func = _frames_methods[name]
-            #def callable(*args, **kwargs):
-            #    return func(self.obj, *args, **kwargs)
-            #
-            #return callable
-            #return lambda: _frames_methods[name](self.obj, *args, **kwargs)
 
         if self._is_group_by and name in _groups_methods:
             return self.create_callable(_groups_methods[name])
-            #return lambda: _groups_methods[name](self.obj, *args, **kwargs)
 
         return self.obj.__getattribute__(*args, **kwargs)
-
-
-# class Bamboo(object):
-#     """
-#     A class designed to wrap a Pandas object and dispatch
-#     bamboo functions (when available).  Else, it falls
-#     back to the object's original methods
-#     """
-
-#     def __init__(self, obj):
-#         self.obj = obj
-
-#     def foo(self):
-#         print "Foo"
-
-#     def __getattr__(self, *args, **kwargs):
-
-#         name = args[0]
-
-#         if isinstance(obj, pandas.DataFrame) and name in _frames_methods:
-#             return _frames_methods[name]
-
-#         if isinstance(obj, pandas.GroupBy) and name in _groupes_methods:
-#             return _groupes_methods[name]
-
-#         return self.obj.__getattribute__(*args, **kwargs)
-
-
-# #        if name in ['head']:
-# #            return lambda : head(self.obj)
-# #        else:
-
