2f54a36c3727c6c10e0aec92afa345328cffb0af
George Lewis <ghl227@gmail.com>
2014-09-13 19:49:10 -0700

    Update wrap tests

--- tests/test_wrap.py
+++ tests/test_wrap.py
@@ -10,7 +10,7 @@ def test_wrap():
 
     df = create_test_df_v2()
 
-    wrap(df).groupby('group') \
+    return wrap(df).groupby('group') \
         .filter_groups(lambda x: x['group'].mean() > 0) \
         .sorted_groups(lambda x: x['feature2'].mean()) \
         .map_groups(lambda x: x['feature1'].mean()) \
@@ -22,40 +22,40 @@ def test_filter_groups():
 
     df = create_test_df_v2()
 
-    wrap(df)\
-    .groupby('group')\
-    .filter_groups(lambda x: x in (0, 1), on_index=True)\
-    .feature1\
-    .value_counts()
+    return wrap(df)\
+        .groupby('group')\
+        .filter_groups(lambda x: x in (0, 1), on_index=True)\
+        .feature1\
+        .value_counts()
 
 
 def test_filter_groups_hist():
 
     df = create_test_df_v2()
 
-    wrap(df)\
-    .groupby('group')\
-    .filter_groups(lambda x: x in (0, 1, 2), on_index=True)\
-    .feature1\
-    .hist(alpha=0.5, normed=True, bins=np.arange(0, 20, 2))
+    return wrap(df)\
+        .groupby('group')\
+        .filter_groups(lambda x: x in (0, 1, 2), on_index=True)\
+        .feature1\
+        .hist(alpha=0.5, normed=True, bins=np.arange(0, 20, 2))
 
 
 def test_sorted_groups():
 
     df = create_test_df_v2()
 
-    wrap(df)\
-    .groupby(df['group'])\
-    .sorted_groups(lambda x: x.feature2.mean())\
-    .feature1.mean()\
-    .head()
+    return wrap(df)\
+        .groupby(df['group'])\
+        .sorted_groups(lambda x: x.feature2.mean())\
+        .feature1.mean()\
+                 .head()
 
 
 def test_map_groups():
 
     df = create_test_df_v2() 
 
-    wrap(df)\
+    return wrap(df)\
         .groupby(df['group'])\
         .map_groups(lambda x: x.feature1 - x.feature2, name='feature1_minus_feature2')\
         .mean()\
@@ -64,12 +64,12 @@ def test_map_groups():
 
 def test_apply_groups():
 
-    df = create_test_df_v2()    
+    df = create_test_df_v2()
 
     def thing(x):
         return x.feature1 + x.feature2
 
-    wrap(df)\
+    return wrap(df)\
         .groupby('group')\
         .apply_groups(thing, lambda x: x.feature1 - x.feature2)\
         .mean()\
