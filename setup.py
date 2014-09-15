9390585aa9148e2765e6b378c6703dbda9cb0bb1
George Lewis <ghl227@gmail.com>
2014-09-14 19:18:57 -0700

    Add dependencies

--- setup.py
+++ setup.py
@@ -19,12 +19,15 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="bamboo",
+    packages=["bamboo"],
     version='0.1',
     author="George Herbert Lewis",
     author_email="ghl227@gmail.com",
-    packages=["bamboo"],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
     description="Data manipulation for python using Pandas",
     url = 'https://github.com/ghl3/bamboo',
     download_url = 'https://github.com/ghl3/bamboo/tarball/0.1',
+    install_requires=[
+        'pandas', 'numpy', 'matplotlib'
+    ],
 )
