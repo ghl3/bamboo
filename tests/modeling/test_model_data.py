ee56de9e0f2943b3204f387882ef8c793f1b9117
George Lewis <ghl227@gmail.com>
2015-03-02 13:01:32 -0500

    In the middle of updating tests

--- tests/modeling/test_model_data.py
+++ tests/modeling/test_model_data.py
@@ -5,7 +5,7 @@ import pandas
 from nose.tools import *
 from bamboo.core import *
 from bamboo.helpers import *
-import bamboo.ml
+import bamboo.modeling
 
 from pandas.util.testing import assert_frame_equal
 
@@ -46,7 +46,7 @@ def test_split():
 
     random_state = RandomState(12345)
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
 
     train, test = data.train_test_split(random_state=random_state)
 
@@ -60,7 +60,7 @@ def test_split():
 
 def test_balance_truncation():
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
 
     eq_(len(data), 12)
 
@@ -80,7 +80,7 @@ def test_balance_truncation():
 
 def test_balance_sample():
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
 
     eq_(len(data), 12)
 
@@ -101,9 +101,9 @@ def test_balance_sample():
 
 def test_orthogonal():
 
-    dataA = bamboo.ml.ModelingData.from_dataframe(df.iloc[0:6], target='group')
-    dataB = bamboo.ml.ModelingData.from_dataframe(df.iloc[3:9], target='group')
-    dataC = bamboo.ml.ModelingData.from_dataframe(df.iloc[6:12], target='group')
+    dataA = bamboo.modeling.ModelingData.from_dataframe(df.iloc[0:6], target='group')
+    dataB = bamboo.modeling.ModelingData.from_dataframe(df.iloc[3:9], target='group')
+    dataC = bamboo.modeling.ModelingData.from_dataframe(df.iloc[6:12], target='group')
 
     assert(dataA.is_orthogonal(dataC))
     assert(not dataA.is_orthogonal(dataB))
@@ -112,7 +112,7 @@ def test_orthogonal():
 
 def test_numeric_features():
 
-    data = bamboo.ml.ModelingData.from_dataframe(df2, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df2, target='group')
 
     eq_(data.shape(), (12,4))
     assert('feature3' in data.features.columns)
@@ -127,7 +127,7 @@ def test_probas():
 
     clf = LogisticRegression()
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
 
     data.fit(clf)
 
@@ -140,7 +140,7 @@ def test_predict():
 
     reg = LinearRegression()
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
 
     data.fit(reg)
 
@@ -154,11 +154,11 @@ def test_summary():
 
     clf = LogisticRegression()
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
     data.fit(clf)
 
     probas = data.predict_proba(clf)
-    summary = bamboo.ml.ModelingData.get_threshold_summary(probas, 1)
+    summary = bamboo.modeling.ModelingData.get_threshold_summary(probas, 1)
 
     eq_(dict(summary), {'sensiticity': 0.5, 'false_positives': 0, 'f1': 0.6666666666666666, 'precision': 1.0, 'false_negatives': 5, 'true_positive_rate': 0.0, 'specificity': 1.0, 'threshold': 0.5, 'target': 1, 'true_negatives': 0, 'recall': 0.5, 'false_positive_rate': 0.4166666666666667, 'true_positives': 5, 'accuracy': 0.4166666666666667})
 
@@ -167,7 +167,7 @@ def test_classifier_performance_summary():
 
     clf = LogisticRegression()
 
-    data = bamboo.ml.ModelingData.from_dataframe(df, target='group')
+    data = bamboo.modeling.ModelingData.from_dataframe(df, target='group')
     data.fit(clf)
 
     summary = data.get_classifier_performance_summary(clf, 0, thresholds=np.arange(0.0, 1.0, 0.1))
